dc4ea164bba3a97fdc99644551b5e62131d86b41
ruelngatia <ruelngatia@gmail.com>
2022-10-14 09:44:06 +0300

    initial

---
+++ functions.js
@@ -0,0 +1,16 @@
+// the method evaluates JavaScript code represented as a string.
+console.log(eval('789 + 1 '));
+
+// checks if number is a finite number
+
+console.log('is 22/7 finite? :'.concat(isFinite(22/7)))
+
+// parses float to string
+
+console.log(parseFloat('22/7') * 7);
+
+// checks if values is a number
+
+const s = 'Node'
+
+console.log('id value s an number'.concat(isNaN(s)))
